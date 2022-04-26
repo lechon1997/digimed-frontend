c380618c54fc05bd1ea7e1944db437d576f89d27
lechon1997 <odriozola-leo@hotmail.com>
2022-04-26 12:16:50 -0300

    menu centrado,formulario de hu3 centrado,logo arreglado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import img from "../static/imagenes-cards/digimed.png";
 const Header = ({ user, auth }) => {
   return (
     <header className="bg-primary d-flex justify-content-between align-items-center header pe-2">
       <div className="fondo-logo">
         <Link className="navbar-brand" to="/">
-          <img className="imagen-xd" src="./digimed.png" />
+          <img className="imagen-xd" src={img} />
         </Link>
       </div>
       <div>
