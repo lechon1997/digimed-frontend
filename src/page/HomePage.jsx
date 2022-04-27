696d6c53f8639a1e49e7858d579c5dfc6a74d170
mary <mevere66@gmail.com>
2022-04-27 16:32:32 -0500

    boton redireccionar formulario

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import NuevoPacientePage from "./NuevoPacientePage";
 import ListaDeFuncionesPage from "./ListaDeFuncionesPage";
 import ListaDePersonalPage from "./ListaDePersonalPage";
 import VerificarDniPage from "./VerificarDniPage";
 import Menu from "../components/Menu";
 import SintomasPage from "./SintomasPage";
-import DelegarTareasPage from "./DelegarTareasPage";
 import DiagnosticoPage from "./DiagnosticoPage";
 import { useSelector } from "react-redux";
 import * as actions from "../actions/verifyActions";
@@ -15,21 +14,29 @@ import FormularioNuevaFuncionPage from "./FormularioNuevaFuncionPage";
 import ListaDeFunciones from "../components/ListaDeFunciones";
 
 const HomePage = () => {
+  const url = "https://forms.gle/sZPy5oci54wUviU79";
+  const urlChanger = (e) => {
+    e.preventDefault();
+    window.location.assign(url)
+  }
+
   return (
     <div className="menu-home w-100">
-      
+
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/nuevo-paciente" element={<NuevoPacientePage />} />
         <Route path="/lista-de-personal" element={<ListaDePersonalPage />} />
         <Route path="/verificar-dni" element={<VerificarDniPage />} />
         <Route path="/lista-de-funciones" element={<ListaDeFuncionesPage />} />
-        <Route path="/ingresar-funcion" element={<FormularioNuevaFuncionPage />}/>
+        <Route path="/ingresar-funcion" element={<FormularioNuevaFuncionPage />} />
         <Route path="/paciente/sintomas/:id" element={<SintomasPage />} />
         <Route path="/paciente/diagnostico/:id" element={<DiagnosticoPage />} />
         <Route path="/atencion/tratamiento/:idatencion/:idPaciente" element={<TratamientoForm />} />
       </Routes>
-      <button className="btn-redireccion">Encuesta</button>
+      <button className="btn-redireccion"
+        onClick={urlChanger}
+      >Encuesta</button>
     </div>
   );
 };
